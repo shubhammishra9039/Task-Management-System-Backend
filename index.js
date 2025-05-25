3e5f7a45cc9148a3468133a67b6db91164dd66fd
Shubham Mishra <shubhammishra6595@gmail.com>
2025-05-25 10:58:25 +0530

    Debug

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ const frontendBaseURL = process.env.FRONTEND_BASE_URL;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(secret));
-app.use(
-  cors({
-    origin: "*", // A
-    credentials: true,
-  })
-);
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'https://amazing-froyo-135610.netlify.app',
+  credentials: true
+}));
+
 
 
 app.use("/api/v1/user", userRoutes);
